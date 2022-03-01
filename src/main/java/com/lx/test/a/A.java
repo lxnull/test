26823eb0abfa87edeff5388ee43da8256047176c
lx <443199068@qq.com>
2022-03-01 17:49:45 +0800

    first commit

---
+++ src/main/java/com/lx/test/a/A.java
@@ -0,0 +1,8 @@
+package com.lx.test.a;
+
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
---
+++ src/test/java/com/lx/test/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
